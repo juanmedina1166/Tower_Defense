562ff5b74aad0a1f66cea24b9c4efa17419749e3
xaca <and1res@hotmail.com>
2024-05-27 16:53:35 -0500

    Configuracion ventana Game Over

--- Assets/Juego/Scripts/FindeJuego.cs
+++ Assets/Juego/Scripts/FindeJuego.cs
@@ -7,14 +7,16 @@ using UnityEngine.SceneManagement;
 
 public class FindeJuego : MonoBehaviour
 {
-    
+    private bool gameOver = false;
     // Update is called once per frame
     void Update()
     {
-        if (GameManager.Instance.CurrentLives <= 0) 
+        if (GameManager.Instance.CurrentLives <= 0 && !gameOver) 
         {
+            gameOver = true;
             MMSceneLoadingManager.LoadScene("Game Over");
         }
         
     }
+
 }
